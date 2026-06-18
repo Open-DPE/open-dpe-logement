b0bb8581314ba78b5114a73c86fef5be06040788
Adrien Rosi <adrienrosi@gmail.com>
2026-06-18 20:35:50 +0200

    v1.1.0

--- apps/ambassadeurs-renov/src/main.tsx
+++ apps/ambassadeurs-renov/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home } from "./pages/test";
+import { Home } from "./pages/home";
 import { Simulation } from "./pages/simulation";
 import { Ressources } from "./pages/ressources";
 import { MonLogement } from "./pages/mon-logement";
